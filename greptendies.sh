f657f163b8b25ca3c20e34379cd77491fc1473a0
LeelaPakanati <LeelaPakanati.gmail.com>
2019-12-18 21:55:09 -0500

    Combine sed statements

--- greptendies.sh
+++ greptendies.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 cd $(dirname "$0")
-curl $(cat menusrc) -s | grep -f foods - | sed 's/^\s+//g' -E | sed 's/\n([^$])/, \1/g' -Ez | xargs -r -I{} mutt -s "{} at the bone today" -b $(cat emaillist)
-cd -
+curl $(cat menusrc) -s | grep -f foods - | sed 's/^\s+//g;s/\n\s*([^$])/, \1/g' -Ez \
+	| xargs -r -I{} mutt -s "{} at the bone today" -b $(cat emaillist)
+cd - > /dev/null
